424fa45ce3121ba7989d9078a145ee07ad242540
Jakob Ackermann <das7pad@outlook.com>
2019-04-18 23:11:25 +0200

    [generator] es: update the nvm rc
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- generator/project/es.py
+++ generator/project/es.py
@@ -3,3 +3,23 @@ from generator.project import Project
 
 class ESProject(Project):
     language = 'es'
+
+    def _update_nvmrc(self):
+        nvm_rc = self._path / '.nvmrc'
+
+        current_node_version = nvm_rc.read_text().strip()
+        new_node_version = self['node_version']
+
+        if current_node_version == new_node_version:
+            return False
+
+        self._changed = True
+        nvm_rc.write_text(new_node_version + '\n')
+        return True
+
+    def process(
+        self,
+    ):
+        self._update_nvmrc()
+
+        return super().process()
