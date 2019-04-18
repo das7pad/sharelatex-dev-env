1f72ba7c8186beb12f69ea7cd22d5cad66c88631
Jakob Ackermann <das7pad@outlook.com>
2019-04-18 15:09:20 +0200

    [generator] add the entry point
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- generator/__main__.py
+++ generator/__main__.py
@@ -1,11 +1,28 @@
+import argparse
 import pathlib
-import sys
+import typing
 
 from generator.project import Project
 
 
+def get_args(args: typing.Optional[typing.List[str]] = None):
+    parser = argparse.ArgumentParser(
+        'generator',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        'path',
+        nargs='+',
+        type=pathlib.Path,
+        help='One or more project paths',
+    )
+
+    return parser.parse_args(args)
+
+
 def main(args):
-    for raw_path in args:
+    for raw_path in args.path:
         path = pathlib.Path(raw_path)
 
         project = Project.from_path(
@@ -15,4 +32,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(get_args())
