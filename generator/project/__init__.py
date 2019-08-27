d792efd9bbb10eb8c75d32bb2b218e44d44e1d1f
Jakob Ackermann <das7pad@outlook.com>
2019-08-27 12:18:53 +0200

    [templates] es: make: simplify the conditionally noop targets
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- generator/project/__init__.py
+++ generator/project/__init__.py
@@ -308,6 +308,7 @@ class Project:
             'has_entrypoint': 'entrypoint.sh',
             'has_setup_env': 'setup_env.sh',
             'has_acceptance_tests': 'test/acceptance',
+            'has_pre_test_acceptance': 'test/acceptance/scripts/pre-run',
             'has_unit_tests': 'test/unit',
         }
 
