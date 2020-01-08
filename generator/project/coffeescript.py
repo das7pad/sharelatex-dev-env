2e5c1569c815da1daca6ddd7a674cdb8cd2d7936
Jakob Ackermann <das7pad@outlook.com>
2020-01-08 18:01:48 +0100

    [generator] coffee: predict the paths for compiled sources
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- generator/project/coffeescript.py
+++ generator/project/coffeescript.py
@@ -9,6 +9,11 @@ class CoffeeScriptProject(ESProject):
     ):
         files = super()._get_possible_project_files()
         files.update({
+            'has_app_js': 'app.coffee',
+            'has_app_src': 'app/coffee',
+            'has_acceptance_test_bootstrap': 'test/acceptance/bootstrap.coffee',
             'has_acceptance_test_init': 'test/acceptance/coffee/Init.coffee',
+            'has_index_js': 'index.coffee',
+            'has_unit_test_bootstrap': 'test/unit/bootstrap.coffee',
         })
         return files
