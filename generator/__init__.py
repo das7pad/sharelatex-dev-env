7d9c18ea0bced6954fcaecaaa7e6064692f4c186
Jakob Ackermann <das7pad@outlook.com>
2021-01-17 15:28:53 +0100

    [misc] drop support for coffeescript

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,4 +1,3 @@
 from generator.project import Project
 from generator.project.es import ESProject
-from generator.project.coffeescript import CoffeeScriptProject
 from generator.project.runner import RunnerProject
